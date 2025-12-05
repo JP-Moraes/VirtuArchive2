af106b1b8fdcc2af6c39d094df58ebfa7bc8ccb0
JP-Moraes <foxcraft2030@gmail.com>
2025-12-05 09:26:34 -0300

    Update Navbar title from 'Biblioteca de Jogos' to 'VirtuArchive'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <div>🎮 Biblioteca de Jogos</div>
+      <div> VirtuArchive </div>
       <ul className={styles.navLinks}>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/jogo1">CyberRunner</Link></li>
